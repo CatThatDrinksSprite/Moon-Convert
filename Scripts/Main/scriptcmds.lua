a350ffd65e64dd1470389901ef91298862851d01
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-09-14 15:44:49 -0400

    Update scriptcmds.lua

--- Scripts/Main/scriptcmds.lua
+++ Scripts/Main/scriptcmds.lua
@@ -12,4 +12,5 @@ scriptcmds = {
   ["chips"] = function()
     sendNotification("Moon Convert", "Running!", 7)
     loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/raw/main/Scripts/Converts/chips.lua", true))()
+  end,
 }
