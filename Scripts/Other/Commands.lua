34a8daa5c2c63598dff7c3c1ee1b3ef0f52bdf2b
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-09-14 14:05:26 -0400

    Update Commands.lua

--- Scripts/Other/Commands.lua
+++ Scripts/Other/Commands.lua
@@ -1,8 +1,8 @@
-loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/blob/main/Scripts/Other/sendNotification.lua", true))()
+loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/raw/main/Scripts/Other/sendNotification.lua", true))()
 
 scriptcmds = {
   ["reanimate"] = function()
     sendNotification("Moon Convert", "Reanimating!", 7)
-    loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/blob/main/Scripts/Reanimation/MyWorld.lua", true))()
+    loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/raw/main/Scripts/Reanimation/MyWorld.lua", true))()
   end
 }
