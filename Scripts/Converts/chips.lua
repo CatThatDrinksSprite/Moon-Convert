c6d0737c90a2eddee2a03667c804dceb36092352
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-09-14 15:19:27 -0400

    Update chips.lua

--- Scripts/Converts/chips.lua
+++ Scripts/Converts/chips.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/blob/main/Scripts/Other/LoadLibrary.lua", true))()
+loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/raw/main/Scripts/Other/LoadLibrary.lua", true))()
 
 -----------------------
 --[[ Name : Chips ]]--
