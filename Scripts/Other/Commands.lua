8544aecff903fcecb76ad354caeb846c063b891f
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-09-14 14:06:05 -0400

    Delete Scripts/Other/Commands.lua

--- Scripts/Other/Commands.lua
+++
@@ -1,8 +0,0 @@
-loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/raw/main/Scripts/Other/sendNotification.lua", true))()
-
-scriptcmds = {
-  ["reanimate"] = function()
-    sendNotification("Moon Convert", "Reanimating!", 7)
-    loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/raw/main/Scripts/Reanimation/MyWorld.lua", true))()
-  end
-}
