fcd2401c38049b6a4af4d4d02f08aff9d2a79aa8
luanecoarc <100092400+luanecoarc@users.noreply.github.com>
2025-09-25 06:51:53 -0400

    Update scriptcmds.lua

--- Scripts/Main/scriptcmds.lua
+++ Scripts/Main/scriptcmds.lua
@@ -82,4 +82,12 @@ scriptcmds = {
       game:GetService("TextChatService").TextChannels.RBXSystem:SendAsync("-gh 4964938812")
     end
   end,
+  ["get hats;shopping cart"] = function()
+    sendNotification("Moon Convert", "Getting Hats!", 7)
+    if game.Players.LocalPlayer.Character:FindFirstChild("Model") then
+      sendNotification("Moon Convert", "Please get hats before reanimating", 7)
+    else
+      game:GetService("TextChatService").TextChannels.RBXSystem:SendAsync("-gh 4794163533")
+    end
+  end
 }
