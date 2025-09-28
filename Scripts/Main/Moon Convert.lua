1cd333ea8809d62711da72db160a8936fc4e3284
luanecoarc <100092400+luanecoarc@users.noreply.github.com>
2025-09-28 12:41:46 -0400

    Update Moon Convert.lua

--- Scripts/Main/Moon Convert.lua
+++ Scripts/Main/Moon Convert.lua
@@ -1,5 +1,5 @@
-loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/raw/main/Scripts/Main/scriptcmds.lua", true))()
-loadstring(game:HttpGet("https://github.com/sharkymeowwww/Moon-Convert/raw/main/Scripts/Other/sendNotification.lua", true))()
+loadstring(game:HttpGet("https://github.com/luanecoarc/Moon-Convert/raw/main/Scripts/Main/scriptcmds.lua", true))()
+loadstring(game:HttpGet("https://github.com/luanecoarc/Moon-Convert/raw/main/Scripts/Other/sendNotification.lua", true))()
 print(game.PlaceId)
 if game.PlaceId == 88308889239232 or game.PlaceId == 17574618959 or game.PlaceId == 92637633202893 then
 	print("good")
