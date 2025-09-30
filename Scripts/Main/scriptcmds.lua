ef260b30d87c43decb2f0f9924bc7ff850a48fef
luanecoarc <100092400+luanecoarc@users.noreply.github.com>
2025-09-30 09:31:33 -0400

    Update scriptcmds.lua

--- Scripts/Main/scriptcmds.lua
+++ Scripts/Main/scriptcmds.lua
@@ -101,10 +101,10 @@ scriptcmds = {
       sendNotification("Moon Convert", "Please use command \"reanimate\" first.", 7)
     end
   end,
-  ["doll;9"] = function()
+  ["doll;2"] = function()
     sendNotification("Moon Convert", "Ran!", 7)
     if game.Players.LocalPlayer.Character:FindFirstChild("Model") then
-      loadstring(game:HttpGet("https://github.com/luanecoarc/Moon-Convert/raw/main/Scripts/Converts/doll%3B9.lua", true))()
+      loadstring(game:HttpGet("https://github.com/luanecoarc/Moon-Convert/raw/main/Scripts/Converts/doll%3B2.lua", true))()
     else
       sendNotification("Moon Convert", "Please use command \"reanimate\" first.", 7)
     end
