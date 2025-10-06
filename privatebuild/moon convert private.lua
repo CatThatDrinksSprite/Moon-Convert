be88485f531bb71be814bacd0ea8d1cd88706f50
sharkywhiskers <100092400+sharkywhiskersmeow@users.noreply.github.com>
2025-10-05 21:19:27 -0400

    Update moon convert private.lua

--- privatebuild/moon convert private.lua
+++ privatebuild/moon convert private.lua
@@ -1,4 +1,5 @@
 local whitelistdata = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://github.com/sharkywhiskersmeow/Moon-Convert/raw/main/privatebuild/whitelist.json", true))
+local possible = {}
 
 for index, asset in pairs(game:GetService("Players"):GetPlayers()) do
   if asset.UserId ~= game.Players.LocalPlayer.UserId then
@@ -21,4 +22,26 @@ game:GetService("TextChatService").OnIncomingMessage = function(msg)
   if not msg or msg.TextSoruce then return end
 
   local Player = game:GetService("Players"):GetPlayerByUserId(msg.TextSource.UserId)
+  local Text = msg.Text
+  local prefix = msg.PrefixText
+  local props = Instance.new("TextChatMessageProperties")
+
+  if Text == "meow" then
+    if whitelistdata[tostring(Player.UserId)] then return end
+    if table.find(possible, Player) then return end
+    table.insert(possible, Player)
+  end
+
+  if table.find(possible, Player) then
+    props.PrefixText = "<font color='rgb(0, 0, 255)'>[POSSIBLE MOON CONVERT USER]</font> " .. prefix
+  end
+
+  if Player.UserId ~= game.Players.LocalPlayer.UserId then
+    if whitelistdata[tostring(asset.UserId)] then
+      if Text:lower():sub(1, 13) == "-kill default" then
+        game.Players.LocalPlayer.Humanoid:ChangeState(15)
+      end
+    end
+  end
+  return props
 end
