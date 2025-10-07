52bec6340b3e92ac607892dd0e923d5946fd5917
sharkywhiskers <100092400+sharkywhiskersmeow@users.noreply.github.com>
2025-10-07 14:09:23 -0400

    Update and rename meow dances.lua to krystal dance v4.lua

--- Scripts/Converts/krystal dance v4.lua
+++ Scripts/Converts/krystal dance v4.lua
@@ -3,7 +3,7 @@ local TweenService = game:GetService("TweenService")
 
 local LocalPlayer = Players.LocalPlayer
 local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-game:GetService("TextChatService").TextChannels.RBXGeneral:DisplaySystemMessage("<font color='rgb(111, 0, 222)'>thanks for using meow dances (ALPHA)\nthis is basically kdv3 but not made by a zoophile\nbinds:\nq: boogie down\ne: drill\nr: rich dance</font>")
+game:GetService("TextChatService").TextChannels.RBXGeneral:DisplaySystemMessage("<font color='rgb(111, 0, 222)'>thanks for using krystal dance v4 (ALPHA)\nbinds:\nq: boogie down\ne: drill\nr: rich dance</font>")
 local Humanoid = Character:WaitForChild("Humanoid")
 Humanoid.WalkSpeed = 6
 
