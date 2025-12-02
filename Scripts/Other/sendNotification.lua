96a366095b3c496bd0294545dd6cd1db09d62028
CatThatDrinksSprite <100092400+CatThatDrinksSprite@users.noreply.github.com>
2025-12-02 07:29:01 -0500

    Update sendNotification.lua

--- Scripts/Other/sendNotification.lua
+++ Scripts/Other/sendNotification.lua
@@ -1,3 +1,8 @@
+if not isfile("ScriptBase/notification.mp3") then
+	sendNotification("Moon Convert", "Downloading \"ScriptBase/notification.mp3\"", 7)
+	writefile("ScriptBase/notification.mp3", game:HttpGet("https://github.com/CatThatDrinksSprite/Moon-Convert/raw/main/ScriptBase/notification.mp3", true))
+end
+
 function sendNotification(Title: string, Text: string, Duration: number)
 	game:GetService("StarterGui"):SetCore("SendNotification", {
 		Title = Title;
@@ -6,7 +11,7 @@ function sendNotification(Title: string, Text: string, Duration: number)
 	})
 	task.delay(0,function()
 		local notif = Instance.new("Sound")
-		notif.SoundId = "rbxassetid://7518627362"
+		notif.SoundId = getcustomasset("ScriptBase/notification.mp3")
 		notif.Volume = 0.7
 		game:GetService("SoundService"):PlayLocalSound(notif)
 		task.delay(5, function()
